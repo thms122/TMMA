d5fa720a87079f835cae3810b2b42abe2b0cbef6
thms122 <thomas_volk@outlook.de>
2025-11-11 09:32:21 +0100

    variable disk space

--- profile.py
+++ profile.py
@@ -1,9 +1,9 @@
 """
-
 Each node:
 - Boots Ubuntu 22.04 LTS
 - Clones a git repository to /local/repository
 - Runs colloid_startup.sh from that repository
+- Optionally attaches a temporary filesystem (/mydata) of requested size
 """
 
 import geni.portal as portal
@@ -11,41 +11,48 @@ import geni.rspec.pg as pg
 
 # Create portal context
 pc = portal.Context()
-
-# Create request RSpec
 request = pc.makeRequestRSpec()
-pc.defineParameter("nodeCount", "Number of Nodes", portal.ParameterType.INTEGER, 1,
-                   longDescription="If you specify more then one node, " +
-                   "we will create a lan for you.")
+
+# === Parameters ===
+pc.defineParameter("nodeCount", "Number of Nodes",
+                   portal.ParameterType.INTEGER, 1,
+                   longDescription="If you specify more than one node, a LAN will be created.")
+
+pc.defineParameter("tempFileSystemSize", "Temporary Filesystem Size (GB)",
+                   portal.ParameterType.INTEGER, 0,
+                   longDescription="The size in GB of a temporary filesystem to mount on each node. "
+                                   "Set to 0 to skip adding extra storage.")
 
 params = pc.bindParameters()
 
-# Configuration: 5 nodes
+# === Configuration ===
 NUM_NODES = params.nodeCount
-
-# Git repository URL containing profile and setup scripts
+TEMP_SIZE = params.tempFileSystemSize  # in GB
 GIT_REPO_URL = "https://github.com/thms122/TMMA.git"
 
-# Loop to add all nodes
+# === Node setup ===
 for i in range(NUM_NODES):
-    node_name = "node%d" % (i + 1)
+    node_name = f"node{i+1}"
     node = request.RawPC(node_name)
 
-    # Set the OS to Ubuntu 22.04 LTS
-    node.hardware_type = "c220g5"  # choose appropriate hardware type
+    # Set OS and hardware
+    node.hardware_type = "c220g5"  # or another available type on your cluster
     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 
-    # Service: clone git repo
-    clone_command = "git clone %s /local/repository || (cd /local/repository && git pull)" % GIT_REPO_URL
-    node.addService(pg.Execute(shell="sh", command=clone_command))
+    # Optionally add extra temporary filesystem
+    if TEMP_SIZE > 0:
+        bs = node.Blockstore("bs_" + node_name, "/local")
+        bs.size = f"{TEMP_SIZE}GB"
 
-    # Make the startup script executable
-    chmod_command = "sudo chmod +x /local/repository/colloid_startup.sh"
-    node.addService(pg.Execute(shell="sh", command=chmod_command))
+    # Clone and run repo
+    clone_cmd = f"git clone {GIT_REPO_URL} /local/repository || (cd /local/repository && git pull)"
+    node.addService(pg.Execute(shell="sh", command=clone_cmd))
 
-    # Service: run colloid_startup.sh
-    node.addService(pg.Execute(shell="sh", command="/local/repository/colloid_startup.sh"))
+    chmod_cmd = "sudo chmod +x /local/repository/colloid_startup.sh"
+    node.addService(pg.Execute(shell="sh", command=chmod_cmd))
 
-# Print the RSpec
+    run_cmd = "/local/repository/colloid_startup.sh"
+    node.addService(pg.Execute(shell="sh", command=run_cmd))
+
+# === Print the RSpec ===
 pc.printRequestRSpec(request)
-
