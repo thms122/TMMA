d6b22048ce3555abbdfb8a463f1be21c4e908be6
thms122 <thomas_volk@outlook.de>
2025-12-05 13:19:25 +0100

    dlrm

--- run_dlrm.sh
+++ run_dlrm.sh
@@ -18,7 +18,7 @@ sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
 cat /proc/vmstat | grep numa_pages_migrated
 cat /proc/vmstat | grep pgpromote_success
 cat /proc/vmstat | grep nr_active_file
-sudo /usr/bin/time --verbose /users/thmsvlk/bin/perf stat -a --per-socket -e dTLB-load-misses,dTLB-loads,dTLB-store-misses,dTLB-stores,cache-misses,cache-references,bus-cycles -- taskset -c 0,1,2,3,4,5,6,7 python dlrm_s_pytorch.py --mini-batch-size=2048 --test-mini-batch-size=16384 --test-num-workers=0 --num-batches=400 --data-generation=random --arch-mlp-bot=2048-2048-512 --arch-mlp-top=1024-1024-1024-1 --arch-sparse-feature-size=512 --arch-embedding-size=1000000-1000000-1000000-1000000-1000000-1000000-1000000 --num-indices-per-lookup=200 --arch-interaction-op=dot --numpy-rand-seed=727
+sudo /usr/bin/time --verbose /users/thmsvlk/bin/perf stat -a --per-socket -e dTLB-load-misses,dTLB-loads,dTLB-store-misses,dTLB-stores,cache-misses,cache-references,bus-cycles -- taskset -c 0,1,2,3,4,5,6,7 python /local/dlrm/dlrm_s_pytorch.py --mini-batch-size=2048 --test-mini-batch-size=16384 --test-num-workers=0 --num-batches=400 --data-generation=random --arch-mlp-bot=2048-2048-512 --arch-mlp-top=1024-1024-1024-1 --arch-sparse-feature-size=512 --arch-embedding-size=1000000-1000000-1000000-1000000-1000000-1000000-1000000 --num-indices-per-lookup=200 --arch-interaction-op=dot --numpy-rand-seed=727
 cat /proc/vmstat | grep numa_pages_migrated
 cat /proc/vmstat | grep pgpromote_success
 cat /proc/vmstat | grep nr_active_file
