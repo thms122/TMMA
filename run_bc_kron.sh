11eae459b808ded75efd0630f1c8fba71637540a
thms122 <thomas_volk@outlook.de>
2025-12-05 13:15:54 +0100

    dlrm

--- run_bc_kron.sh
+++ run_bc_kron.sh
@@ -2,7 +2,7 @@ sudo bash /local/repository/config.sh
 cat /proc/vmstat | grep numa_pages_migrated
 cat /proc/vmstat | grep pgpromote_success
 cat /proc/vmstat | grep nr_active_file
-sudo /usr/bin/time --verbose /local/colloid/tpp/linux-6.3/tools/perf/perf stat -a --per-socket -e dTLB-load-misses,dTLB-loads,dTLB-store-misses,dTLB-stores,cache-misses,cache-references,bus-cycles -- taskset -c 0,1,2,3,4,5,6,7 /local/gapbs/bc -f /local/gapbs/benchmark/graphs/kron.sg -n 20
+sudo /usr/bin/time --verbose /local/colloid/tpp/linux-6.3/tools/perf/perf stat -a --per-socket -e dTLB-load-misses,dTLB-loads,dTLB-store-misses,dTLB-stores,cache-misses,cache-references,bus-cycles -- taskset -c 0,1,2,3,4,5,6,7 python dlrm_s_pytorch.py --mini-batch-size=2048 --test-mini-batch-size=16384 --test-num-workers=0 --num-batches=400 --data-generation=random --arch-mlp-bot=2048-2048-512 --arch-mlp-top=1024-1024-1024-1 --arch-sparse-feature-size=512 --arch-embedding-size=1000000-1000000-1000000-1000000-1000000-1000000-1000000 --num-indices-per-lookup=200 --arch-interaction-op=dot --numpy-rand-seed=727
 cat /proc/vmstat | grep numa_pages_migrated
 cat /proc/vmstat | grep pgpromote_success
 cat /proc/vmstat | grep nr_active_file
