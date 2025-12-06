d0bbd8d926f50a54dc4b1eac189a0b908bec42bc
thms122 <thomas_volk@outlook.de>
2025-12-06 01:23:40 +0100

    bootloop break:

--- run_dlrm_auto.sh
+++ run_dlrm_auto.sh
@@ -206,7 +206,7 @@ for (( id = start_index; id < TOTAL; id++ )); do
             log "Wrote initial checkpoint -1 to indicate no tasks completed."
         fi
         sleep 3
-        sudo reboot
+        #sudo reboot
         exit 0
     fi
 
@@ -215,7 +215,7 @@ for (( id = start_index; id < TOTAL; id++ )); do
     log "=== Finished $bench | THP=$thp | DEFRAG=$defrag | WM=$wm (task id=$id) ==="
     log "Rebooting before next benchmark (or exiting if none left)."
     sleep 5
-    sudo reboot
+    #sudo reboot
     exit 0
 done
 
