1a92d79f0ebfc3aaef9a0891d56f529d5db46e50
thms122 <thomas_volk@outlook.de>
2025-12-07 11:30:20 +0100

    lol

--- run_dlrm_auto.sh
+++ run_dlrm_auto.sh
@@ -168,9 +168,11 @@ for (( id = start_index; id < TOTAL; id++ )); do
     cat /proc/vmstat | grep nr_active_file          2>&1 | tee -a "$logfile"
 
     # Run the benchmark under perf (time + perf stat). Put the whole invocation in a subshell to capture exit status.
-    sudo /usr/bin/time --verbose /local/colloid/tpp/linux-6.3/tools/perf/perf stat -a --per-socket \
-        -e dTLB-load-misses,dTLB-loads,dTLB-store-misses,dTLB-stores,cache-misses,cache-references,bus-cycles \
-        -- taskset -c 0,1,2,3,4,5,6,7 python -c "$cmd" 2>&1 | tee -a "$logfile"
+    sudo /usr/bin/time --verbose \
+    /local/colloid/tpp/linux-6.3/tools/perf/perf stat -a --per-socket \
+    -e dTLB-load-misses,dTLB-loads,dTLB-store-misses,dTLB-stores,cache-misses,cache-references,bus-cycles \
+    -- taskset -c 0,1,2,3,4,5,6,7 python $cmd 2>&1 | tee -a "$logfile"
+
     exit_status=${PIPESTATUS[0]}
     echo "Exit status: $exit_status" | tee -a "$logfile"
 
