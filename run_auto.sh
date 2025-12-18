f8fb3d5bd5680c2ae76a8d51d722180b62f23245
thms122 <thomas_volk@outlook.de>
2025-12-18 21:10:15 +0100

    test

--- run_auto.sh
+++ run_auto.sh
@@ -173,7 +173,7 @@ for (( id=start_index; id<TOTAL; id++ )); do
     sudo /usr/bin/time --verbose \
     /local/colloid/tpp/linux-6.3/tools/perf/perf stat -a --per-socket \
     -e dTLB-load-misses,dTLB-loads,dTLB-store-misses,dTLB-stores,cache-misses,cache-references,bus-cycles \
-    -- taskset -c 0,1,2,3,4,5,6,7  "$cmd" 2>&1 | sudo tee -a "$logfile"
+    -- taskset -c 0,1,2,3,4,5,6,7 bash -c "$cmd" 2>&1 | sudo tee -a "$logfile"
 
     exit_status=${PIPESTATUS[0]}
     echo "Exit status: $exit_status" | sudo tee -a "$logfile"
