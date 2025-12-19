d148affaa433536ce3c16fe6b2c42c91b99cdd3a
thms122 <thomas_volk@outlook.de>
2025-12-19 13:56:10 +0100

    new

--- bench_dlrm.sh
+++ bench_dlrm.sh
@@ -36,7 +36,7 @@ BENCH_NAMES=(
 )
 
 BENCH_CMDS=(
-  "/local/dlrm/dlrm_s_pytorch.py --mini-batch-size=128 --test-mini-batch-size=256 --test-num-workers=0 --num-batches=5 --data-generation=random --arch-mlp-bot=2048-2048-512 --arch-mlp-top=1024-1024-1024-1 --arch-sparse-feature-size=512 --arch-embedding-size=1000000-1000000-1000000-1000000-1000000-1000000-1000000 --num-indices-per-lookup=200 --arch-interaction-op=dot --numpy-rand-seed=727"
+  "python /local/dlrm/dlrm_s_pytorch.py --mini-batch-size=128 --test-mini-batch-size=256 --test-num-workers=0 --num-batches=5 --data-generation=random --arch-mlp-bot=2048-2048-512 --arch-mlp-top=1024-1024-1024-1 --arch-sparse-feature-size=512 --arch-embedding-size=1000000-1000000-1000000-1000000-1000000-1000000-1000000 --num-indices-per-lookup=200 --arch-interaction-op=dot --numpy-rand-seed=727"
 )
 
 # -----------------------------------------------------------------------------
@@ -167,7 +167,7 @@ for (( id=start_index; id<TOTAL; id++ )); do
     sudo /usr/bin/time --verbose \
     /local/colloid/tpp/linux-6.3/tools/perf/perf stat -a --per-socket \
     -e dTLB-load-misses,dTLB-loads,dTLB-store-misses,dTLB-stores,cache-misses,cache-references,bus-cycles \
-    -- taskset -c 0,1,2,3,4,5,6,7 python "$cmd" 2>&1 | sudo tee -a "$logfile"
+    -- taskset -c 0,1,2,3,4,5,6,7 bash -c "$cmd" 2>&1 | sudo tee -a "$logfile"
 
     exit_status=${PIPESTATUS[0]}
     echo "Exit status: $exit_status" | sudo tee -a "$logfile"
