e69cc142e3ca7a9119395ff06ed04abcb68243bf
thms122 <thomas_volk@outlook.de>
2025-12-28 15:41:00 +0100

    test

---
+++ run_liblinear.sh
@@ -0,0 +1,15 @@
+sudo bash /local/repository/config.sh
+cat /proc/vmstat | grep numa_pages_migrated
+cat /proc/vmstat | grep pgpromote_success
+cat /proc/vmstat | grep nr_active_file
+sudo /usr/bin/time --verbose /users/thmsvlk/bin/perf stat -a --per-socket -e dTLB-load-misses,dTLB-loads,dTLB-store-misses,dTLB-stores,cache-misses,cache-references,bus-cycles -- taskset -c 0,1,2,3,4,5,6,7 /local/liblinear/train -s 6 /local/liblinear/HIGGS
+cat /proc/vmstat | grep numa_pages_migrated
+cat /proc/vmstat | grep pgpromote_success
+cat /proc/vmstat | grep nr_active_file
+sudo cat /sys/kernel/mm/transparent_hugepage/defrag
+sudo cat /sys/kernel/mm/transparent_hugepage/enabled
+sudo cat /proc/sys/vm/watermark_scale_factor
+sudo cat /proc/sys/vm/zone_reclaim_mode
+sudo cat /proc/sys/vm/swappiness
+ls /sys/devices/virtual/memory_tiering/
+
