fc0a6f78379fc431221a1b3de7a5a80c26094b6c
thms122 <thomas_volk@outlook.de>
2026-01-13 10:54:35 +0100

    t

--- bench_llama.sh
+++ bench_llama.sh
@@ -49,7 +49,7 @@ DEFRAG_FOR_NEVER=("never")
 
 WM_VALUES=(10 100 500 1000)
 VFS_VALUES=(100)
-SWAP_VALUES=(1)
+SWAP_VALUES=(5)
 
 # -----------------------------------------------------------------------------
 # SYSTEM TUNING HELPERS
