5a379593647fecd2c2dce10d1a69f88d74ddb2c6
thms122 <thomas_volk@outlook.de>
2026-01-24 18:38:41 +0100

    r

--- linux-1/bench_gapbs.sh
+++ linux-1/bench_gapbs.sh
@@ -52,7 +52,7 @@ DEFRAG_FOR_NEVER=("never")
 
 WM_VALUES=(10 100 500 1000)
 VFS_VALUES=(100)
-SWAP_VALUES=(60)
+SWAP_VALUES=(0 1 100)
 
 # -----------------------------------------------------------------------------
 # SYSTEM TUNING HELPERS
