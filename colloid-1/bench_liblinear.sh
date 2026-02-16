0bfa523979c97e75dde000f38b111c5588f0ed1b
thms122 <thomas_volk@outlook.de>
2026-02-16 20:24:07 +0100

    2

--- colloid-1/bench_liblinear.sh
+++ colloid-1/bench_liblinear.sh
@@ -48,8 +48,8 @@ DEFRAG_FOR_ALWAYS=("always" "never" "defer+madvise" "madvise")
 DEFRAG_FOR_NEVER=("never")
 
 WM_VALUES=(10 100 500 1000)
-VFS_VALUES=(0 50 100 500)
-SWAP_VALUES=(0 10 60 100)
+VFS_VALUES=(100)
+SWAP_VALUES=(0 60 100)
 
 # -----------------------------------------------------------------------------
 # SYSTEM TUNING HELPERS
