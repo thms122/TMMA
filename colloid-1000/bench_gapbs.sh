5242065dd493d619eb103ad799e37caf7fc26d9c
thms122 <thomas_volk@outlook.de>
2026-03-01 11:16:31 +0100

    tets

--- colloid-1000/bench_gapbs.sh
+++ colloid-1000/bench_gapbs.sh
@@ -13,7 +13,7 @@ set -u
 # -----------------------------------------------------------------------------
 # LOGGING / CHECKPOINT
 # -----------------------------------------------------------------------------
-LOGDIR="/local/logs/gapbs_logs"
+LOGDIR="/local/logs/gapbs_logs_temp"
 mkdir -p "$LOGDIR"
 CHECKPOINT="$LOGDIR/checkpoint.idx"
 
@@ -45,14 +45,14 @@ BENCH_CMDS=(
 # -----------------------------------------------------------------------------
 # PARAMETER SWEEPS
 # -----------------------------------------------------------------------------
-THP_MODES=("never" "always")
+THP_MODES=("madvise")
 
-DEFRAG_FOR_ALWAYS=("always" "never" "defer+madvise" "madvise")
+DEFRAG_FOR_ALWAYS=("madvise")
 DEFRAG_FOR_NEVER=("never")
 
-WM_VALUES=(10 100 500 1000)
-VFS_VALUES=(0 50 100 500)
-SWAP_VALUES=(0 1 10 60 100)
+WM_VALUES=(10)
+VFS_VALUES=(100)
+SWAP_VALUES=(60)
 
 # -----------------------------------------------------------------------------
 # SYSTEM TUNING HELPERS
