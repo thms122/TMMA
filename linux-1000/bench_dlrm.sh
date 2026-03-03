1a6d1c22619fe83fea0377e124ba1fc49acea685
thms122 <thomas_volk@outlook.de>
2026-03-03 18:23:01 +0100

    d

--- linux-1000/bench_dlrm.sh
+++ linux-1000/bench_dlrm.sh
@@ -13,7 +13,7 @@ set -u
 # -----------------------------------------------------------------------------
 # LOGGING / CHECKPOINT
 # -----------------------------------------------------------------------------
-LOGDIR="/local/logs/16gb_dlrm_logs_l1000"
+LOGDIR="/local/logs/dlrm_logs_l1000"
 mkdir -p "$LOGDIR"
 CHECKPOINT="$LOGDIR/checkpoint.idx"
 
