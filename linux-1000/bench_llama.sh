4efb19d44dbddc7632f05aa040460002ff571337
thms122 <thomas_volk@outlook.de>
2026-03-04 18:02:18 +0100

    d

--- linux-1000/bench_llama.sh
+++ linux-1000/bench_llama.sh
@@ -44,13 +44,13 @@ BENCH_CMDS=(
 # -----------------------------------------------------------------------------
 THP_MODES=("never" "always")
 
-DEFRAG_FOR_ALWAYS=("always" "never" "defer+madvise")
+DEFRAG_FOR_ALWAYS=("defer+madvise")
 DEFRAG_FOR_NEVER=("never")
 
-WM_VALUES=(10)
+WM_VALUES=(10 100 500 1000)
 VFS_VALUES=(100)
 SWAP_VALUES=(60)
-ZONE_VALUES=(1 3 7)
+ZONE_VALUES=(0)
 
 # -----------------------------------------------------------------------------
 # SYSTEM TUNING HELPERS
