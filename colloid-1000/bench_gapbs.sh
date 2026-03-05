d489f5678afb32130ff1484c749509a14739193a
thms122 <thomas_volk@outlook.de>
2026-03-05 20:15:58 +0100

    d

--- colloid-1000/bench_gapbs.sh
+++ colloid-1000/bench_gapbs.sh
@@ -13,7 +13,7 @@ set -u
 # -----------------------------------------------------------------------------
 # LOGGING / CHECKPOINT
 # -----------------------------------------------------------------------------
-LOGDIR="/local/logs/gapbs_logs_cl1"
+LOGDIR="/local/logs/16gb_gapbs_logs_cl1000"
 mkdir -p "$LOGDIR"
 CHECKPOINT="$LOGDIR/checkpoint.idx"
 
@@ -50,9 +50,9 @@ THP_MODES=("never" "always")
 DEFRAG_FOR_ALWAYS=("always" "never" "defer+madvise")
 DEFRAG_FOR_NEVER=("never")
 
-WM_VALUES=(10)
+WM_VALUES=(10 100 500 1000)
 VFS_VALUES=(100)
-SWAP_VALUES=(0)
+SWAP_VALUES=(60)
 ZONE_VALUES=(0)
 
 # -----------------------------------------------------------------------------
@@ -98,7 +98,7 @@ run_repo_config() {
     sudo sh -c 'echo 1 > /sys/kernel/mm/numa/demotion_enabled' || true
     sudo sh -c 'echo 6 > /proc/sys/kernel/numa_balancing' || true
 
-    sudo sh -c "echo 1 > /sys/kernel/debug/sched/numa_balancing/hot_threshold_ms" || true
+    sudo sh -c "echo 1000 > /sys/kernel/debug/sched/numa_balancing/hot_threshold_ms" || true
 
     sudo swapoff -a || true
     sudo sync
@@ -207,7 +207,7 @@ for (( id=start_index; id<TOTAL; id++ )); do
 
     echo "numahot_threshold:"       | sudo tee -a "$logfile"
     sudo cat /sys/kernel/debug/sched/numa_balancing/hot_threshold_ms                      2>&1 | sudo tee -a "$logfile"
-    
+
     ls /sys/devices/virtual/memory_tiering/                      2>&1 | sudo tee -a "$logfile"
 
     if [ "$exit_status" -ne 0 ]; then
