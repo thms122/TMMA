88981984c3afc4bac1b403702566161cbe8aa0be
thms122 <thomas_volk@outlook.de>
2026-03-08 10:39:05 +0100

    e

--- linux-1000/bench_llama.sh
+++ linux-1000/bench_llama.sh
@@ -47,9 +47,9 @@ THP_MODES=("never" "always")
 DEFRAG_FOR_ALWAYS=("always" "never" "defer+madvise")
 DEFRAG_FOR_NEVER=("never")
 
-WM_VALUES=(10 100 500 1000)
+WM_VALUES=(10)
 VFS_VALUES=(100)
-SWAP_VALUES=(60)
+SWAP_VALUES=(0 1 10 100)
 ZONE_VALUES=(0)
 
 # -----------------------------------------------------------------------------
